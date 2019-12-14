4d4e9b5220c18e3bf20879ebe72204a74d7edb07
Cedar <alexianfireflies@gmail.com>
2019-12-14 09:33:59 -0800

    Updates

---
+++ precache-manifest.fa7d801739ee8a3b0df3bf37e11bdab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4113405f09374a103ad1d93a28c276db",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "145845d21b1e7bbd7f39",
+    "url": "/clicky-game/static/css/main.cdac0c32.chunk.css"
+  },
+  {
+    "revision": "de4e21ca5f4be8b5c9dd",
+    "url": "/clicky-game/static/js/2.000a0507.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.000a0507.chunk.js.LICENSE"
+  },
+  {
+    "revision": "145845d21b1e7bbd7f39",
+    "url": "/clicky-game/static/js/main.143fbda1.chunk.js"
+  },
+  {
+    "revision": "602e383eb8f464a5ae82",
+    "url": "/clicky-game/static/js/runtime-main.85d45eb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
